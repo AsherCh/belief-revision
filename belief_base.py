9be8ff04a050ac0fb34c3c1b68529d03d0f21e72
Maggie <maki.lapcik@gmail.com>
2024-04-20 09:42:50 +0200

    adding brackets around b in a->b if b is conjunction or disjunction Fixing bug: a->b&c should translate to ~a|(b&c), brackets were added around b&c

--- belief_base.py
+++ belief_base.py
@@ -63,14 +63,38 @@ class BeliefBasePriority:
                 # split implication into two parts, finding the first occurrence of "->"
                 a, b = belief.split("->", 1)
 
+                # if b has more than one character, it should go into ()
+                # example: "a -> b & c" -> "a -> (b & c)" -> "~a | (b & c)"
+                # & has priority over ->, priority should be kept when changing -> to |
+                # if b is more than one character and has no brackets, add brackets
+                # if first character of b is not "("
+                # if b has any character from the set {"&", "|"}
+
+                if len(b) > 1:
+                    # iterate over a from right to left
+                    bracket = 0
+                    bracket_break = len(b)
+
+                    for j, char in enumerate(b):
+                        if bracket < 0:
+                            bracket_break = j
+                            break
+                        if b[j] == "(":
+                            bracket += 1
+                        if b[j] == ")":
+                            bracket -= 1
+                    bracket_break -= 1
+                    # save a before bracket_break and after bracket_break index into separate variables
+                    b_before = b[:bracket_break]
+                    b_after = b[bracket_break:]
+
+                if ("&" in b_before or "|" in b_before) and (b_before[0] != "(" and b_before[-1] != ")"):
+                    b_before = f"({b_before})"
+                    b = f"{b_before}{b_after}"
+
+
                 # if a has more than one character
                 if len(a) > 1:
-                    # adding brakcets before and after a if there are any
-                    # example: a = "p&q" -> "(p&q)"
-                    # check if there are any ")" in a
-                    if not ")" in a:
-                        a = f"({a})"
-
                     # iterate over a from right to left
                     bracket = 0
                     bracket_break = -1
@@ -86,17 +110,19 @@ class BeliefBasePriority:
                     # save a before bracket_break and after bracket_break index into separate variables
                     a_before = a[:bracket_break]
                     a_after = a[bracket_break:]
-                    self.beliefs[i] = (priority, f"{a_before}~{a_after}|{b}")
+
+                    if (a_after[0] == "(" and a_after[-1] == ")") or len(a_after) == 1:
+                        self.beliefs[i] = (priority, f"{a_before}~{a_after}|{b}")
+
+                    # adding brakcets before and after a if there are any
+                    # example: a = "p&q" -> "(p&q)"
+                    # check if there are any ")" in a
+                    else:
+                        self.beliefs[i] = (priority, f"{a_before}~({a_after})|{b}")
                 else:
                     self.beliefs[i] = (priority, f"~{a}|{b}")
                 belief = self.beliefs[i][1]
         self.print_as_conjunction()
-
-    def count_parentheses(self, belief):
-        # return 0 if the same number of opening and closing parentheses
-        # return < 0 if more closing parentheses than opening parentheses
-        # return > 0 if more opening parentheses than closing parentheses
-        return belief.count("(") - belief.count(")")
 
 
 
