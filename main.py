4ec4307119a97a7a98daa73fa4ee1c71cb2c7717
Maggie <maki.lapcik@gmail.com>
2024-04-24 16:58:46 +0200

    updated main for realistic interaction with user

--- main.py
+++ main.py
@@ -12,7 +12,51 @@ if __name__ == "__main__":
             print("Enter the expression")
             get_expression()
 
-    print("Enter your belief")
-    expression = get_expression()  # Use this funcction to get the expression
+    print("Belief revision system")
+    print("Enter your initial belief:")
+    expression = get_expression()  # Use this function to get the expression
 
-    print("the expression you entered is valid", expression)
+    while 1:
+        print("What would you like to do? Enter an integer (1-3)")
+        print("1 - Check logical entailment")
+        print("2 - Contraction of belief base")
+        print("3 - Expansion of belief base")
+
+        choice = input()
+        is_correct = False
+        while not is_correct:
+            # check if the input is an integer between 1 and 3
+            if choice.isdigit() and 1 <= int(choice) <= 3:
+                choice = int(choice)
+                is_correct = True
+            else:
+                print("Invalid input. Please enter an integer between 1 and 3")
+                choice = input()
+
+        text = "Please enter expression that you would like to "
+        if choice == 1:
+            text += "check for logical entailment: "
+        elif choice == 2:
+            text += "contract from the belief base: "
+        else:
+            text += "expand the belief base with: "
+
+        print(text)
+        # this should also validate the input
+        expression = get_expression()
+
+        if choice == 1:
+            print("Checking for logical entailment")
+            # check for logical entailment
+
+        elif choice == 2:
+            print("Contraction of belief base")
+            # contraction of belief base
+
+        else:
+            print("Expansion of belief base")
+            # expansion of belief base
+
+    #print("the expression you entered is valid", expression)
+
+
